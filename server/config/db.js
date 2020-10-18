f8c1ff2db2d47691222f12f6af84c52ea0db10b6
Franz <fcadien1@my.centennialcollege.ca>
2020-10-18 14:59:47 -0400

    Update URI for MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI" : "mongodb://localhost/client"
+    //"URI" : "mongodb://localhost/client"
+    "URI" : "mongodb+srv://franz:BGqcOop4VJXkqLOM@mongodbserver.vggl6.azure.mongodb.net/client?retryWrites=true&w=majority"
 }
